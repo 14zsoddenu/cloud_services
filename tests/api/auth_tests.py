e5ecb51a267855bac51d3ae5d45744d1a1ee1fbb
Oleksandr Kudryavtsev <o.kudryavtsev@dxc.com>
2021-09-12 14:24:52 +0200

    More auth tests

--- tests/api/auth_tests.py
+++ tests/api/auth_tests.py
@@ -8,10 +8,14 @@ from config import BASE_API_URL
 @pytest.mark.parametrize(
     "url,method",
     [
+        ("cards/", "get"),
+        ("cards/1", "get"),
         ("cards/", "post"),
         ("cards/1", "put"),
         ("cards/1", "delete"),
+        ("dishes/", "get"),
         ("dishes/", "post"),
+        ("dishes/1", "get"),
         ("dishes/1", "put"),
         ("dishes/1", "delete"),
         ("manage/add", "post"),
@@ -29,3 +33,23 @@ def unathorized_access_to_private_api_test(url, method, anon_client):
     request_func = requests_methods_map[method]
     response = request_func(full_url, content_type="application/json")
     assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+@pytest.mark.parametrize(
+    "url,method",
+    [
+        ("menu/cards", "get"),
+        ("menu/cards/1", "get"),
+    ],
+)
+def free_access_to_public_api_test(url, method, anon_client):
+    requests_methods_map = {
+        "get": anon_client.get,
+        "post": anon_client.post,
+        "put": anon_client.put,
+        "delete": anon_client.delete,
+    }
+    full_url = f"/{BASE_API_URL}/{url}"
+    request_func = requests_methods_map[method]
+    response = request_func(full_url, content_type="application/json")
+    assert response.status_code != HTTPStatus.UNAUTHORIZED
